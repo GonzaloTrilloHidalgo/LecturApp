4f07b919bc7799235aea01c06503a3aac4db3f55
Gonzalo Trillo <gonzalo.trillo@soltel.es>
2025-04-20 22:53:17 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+
 export default defineConfig({
+  base: '/LecturApp/',
   plugins: [
     react(),
     VitePWA({
